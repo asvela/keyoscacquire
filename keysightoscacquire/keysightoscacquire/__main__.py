009785cffdf641c788806d11baf957f21412b269
Andreas Svela <svela.andreas@gmail.com>
2019-03-07 12:05:21 +0000

    Further package structuring. BEWARE: directory namechange to keyoscacquire needed.

---
+++ keysightoscacquire/keysightoscacquire/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+When module the executed with 'python -m keyoscacquire'
+obtain and save a single trace.
+
+Andreas Svela // 2019
+"""
+
+import keyoscacquire.programmes as acqprog
+
+if __name__ == "__main__":
+    acqprog.get_single_trace()
